395fa92249a8ad24b3cf51ab59006fb1e1353f8c
lautaroliendo <liendolautaro@gmail.com>
2023-05-24 07:50:42 -0300

    Ejercicio 1 realizado

---
+++ javas2.js
@@ -0,0 +1,19 @@
+// 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+// 1- Un prompt que pida la edad
+// 2- Un if 
+// 3- si es true lanzar una alert/document.write
+// 4- un else si no es valida que mande otro alert/document.write
+
+
+// let edad=prompt("Escriba su edad")
+// edad=parseInt(edad);
+
+// if (edad>17) {
+//     console.log("Ya puede conducir")
+// }
+
+// else{
+//     console.log("No puede manejar");
+// }
+
