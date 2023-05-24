e1491fcd9c799acfd12f0835cce45af5b408bd3d
lautaroliendo <liendolautaro@gmail.com>
2023-05-24 08:32:00 -0300

    Ejercicio 2 realizado

--- javas2.js
+++ javas2.js
@@ -17,3 +17,58 @@
 //     console.log("No puede manejar");
 // }
 
+
+// 2----------------------------------------------------------------------------------------------------------------------
+
+// 2- Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+// let nota = parseInt(prompt("Escriba un numero de 0 a 10"))
+
+// switch (nota) {
+//     case 0:
+//     case 1:
+//     case 2:
+//         document.write("Muy deficiente")
+//         break;
+
+//     case 3:
+//     case 4:
+//         document.write("Insuficiente")
+//         break;
+
+//     case 5:
+//     case 6:
+//         document.write("Suficiente")
+//         break;
+
+//     case 7:
+//         document.write("Bien")
+//         break;
+
+//     case 8:
+//     case 9:
+//         document.write("Notable")
+//         break;
+
+
+//     case 10:
+//         document.write("Sobresaliente")
+//         break;
+
+//     default:
+//         document.write("ingrese un numero valido")
+//         break;
+// }
+
+// 3-----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
