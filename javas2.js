3e774a94749306a630d76207e4dfb82acddbf2f4
lautaroliendo <liendolautaro@gmail.com>
2023-05-24 17:57:03 -0300

    Ejercicio 16 realizado

--- javas2.js
+++ javas2.js
@@ -303,5 +303,11 @@
 // }
 // document.write("Cantidad de vocales: " +vocales);
 
-// 15-----------------------------------------------------------------
+// 16-----------------------------------------------------------------
+// 16- Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
+
+// let cadena= prompt("Ingrese el texto");
+// for(let i=cadena.length; i>=0; i--){
+//     document.write(cadena.charAt(i));
+// }
 
